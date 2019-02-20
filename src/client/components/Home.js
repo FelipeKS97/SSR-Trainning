2b117dfe6de2d03299fada639291548238e28dc5
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2019-02-20 10:53:29 -0300

    Adding client webpack configuration, inserting new gitignore entry

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,7 +4,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-             Home Component
+            <h1>Home Component</h1>
+            <button onClick={ ()=> console.log('hi!')}>Press</button>
         </div>
     )
 }
