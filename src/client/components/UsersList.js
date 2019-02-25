2427ff3211da414565b89dea0e55a806c3ad3922
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2019-02-25 15:30:39 -0300

    Introducing react-router-config to the project, loadData and redux configuration loading set

--- src/client/components/UsersList.js
+++ src/client/components/UsersList.js
@@ -33,4 +33,10 @@ function mapStateToProps(state) {
     }
 }
 
+function loadData(store) {
+    return store.dispatch(fetchUsers())
+}
+
+export { loadData };
+
 export default connect(mapStateToProps, { fetchUsers })(UsersList)
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,7 +3,7 @@ import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import Routes from '../client/Routes'
-
+import { renderRoutes } from 'react-router-config'
 
 
 export default (req, store) => {
@@ -11,7 +11,9 @@ export default (req, store) => {
     const content = renderToString(
         <Provider store={store}>
             <StaticRouter location={req.path} context={{}}>
-                <Routes />
+                <div>
+                    {renderRoutes(Routes)}
+                </div>
             </StaticRouter>
         </Provider>
     );
