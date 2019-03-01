bf19973a120bcd0a9eddd19a8d1d322ab58500b7
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2019-03-01 14:05:58 -0300

    Proxying process for authentication initialized, express-http-proxy library add, initial config

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import 'babel-polyfill';
 import express from 'express';
 import createStore from './helpers/createStore'
 import renderer from './helpers/renderer'
+import proxy from 'express-http-proxy'
 import { matchRoutes } from 'react-router-config'
 import Routes from './client/Routes'
 const app = express();
+
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+        opts.header['x-forwarded-host'] = 'localhost:3000';
+        return opts
+    }})
+)
 
 app.use(express.static('public'))
 
